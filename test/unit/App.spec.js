54f964f1363d8c31d46feeb710b74930f0792592
riojack <ayjay122@gmail.com>
2017-02-22 18:34:45 -0600

    Get test-driving workflow going

---
+++ test/unit/App.spec.js
@@ -0,0 +1,18 @@
+import App from '../../src/App';
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+
+describe('App tests', () => {
+    let props,
+        element;
+
+    beforeEach(() => {
+        props = {};
+        element = shallow(<App {...props} />)
+    });
+
+    it('should be a div', () => {
+        expect(element.type()).to.equal('div');
+    });
+});
