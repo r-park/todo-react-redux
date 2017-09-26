cab95aed35c12a9da2b0e59196d2ca910b54405b
Gal Bracha <galbra@gmail.com>
2017-09-26 20:32:43 +0300

    Fixed db url. Tasks are public again

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
   apiKey: 'AIzaSyBXpbOuiLMkc2kmR-AdnG4RlkWeU1IoKgs',
   authDomain: 'doocrate.firebaseapp.com',
-  databaseURL: 'doocrate.firebaseapp.com',
+  databaseURL: 'https://doocrate.firebaseio.com/',
   storageBucket: ''
 };
