a46feda377d0c29e6f9acecec022eac9981cb74c
Gal Bracha <galbra@gmail.com>
2017-10-16 11:42:53 +0300

    added ability to send email - not tested yet

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const functions = require('firebase-functions');
-
-//const admin = require('firebase-admin');
-
-//admin.initializeApp(functions.config().firebase);
+const nodemailer = require('nodemailer');
+const Firestore = require('@google-cloud/firestore');
 
 // Max number of tasks per creator
 const MAX_TASK_PER_CREATOR = 500;
@@ -29,4 +27,62 @@ exports.limitTasksPerCreatorFirestore = functions.firestore.document('/tasks/{ta
       return event.data.ref.delete();
     }
   });
+});
+
+
+//----------------SendEmail----------------
+
+// Configure the email transport using the default SMTP transport and Mailgun.
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `from_email`, `send_notifications`, `email_api_key`, `email_domain` Google Cloud environment variables.
+const shouldSendNotifications = encodeURIComponent(functions.config().send_notifications);
+const fromEmail = encodeURIComponent(functions.config().email.from);
+const emailApiKey = encodeURIComponent(functions.config().email.apikey);
+const emailDomain = encodeURIComponent(functions.config().email.domain);
+
+const emailAuth = {
+  auth: {
+      api_key: emailApiKey,
+      domain: emailDomain
+   }
+ }
+
+const mailTransport = nodemailer.createTransport(emailAuth);
+
+const firestore = new Firestore();
+
+exports.sendEmail = functions.firestore.document('/comments/{commentId}').onWrite(event => {
+  
+  if(!shouldSendNotifications) {
+    console.log('send notifications turned off');
+    return;
+  }
+
+  const comment = event.data.data();
+
+  // Check for deleted comment
+  if(!comment || !comment.taskId) {
+    return
+  }
+
+  firestore.collection('tasks').document(comment.taskId).get().then( task => {
+      console.log(task.creator.id);
+      const toEmail = task.creator.id;
+        const mailOptions = {
+          from: fromEmail,
+          to: toEmail
+        };
+      
+        const shortTitle = task.title.substr(0, 15);
+        mailOptions.subject = `New comment - [${shortTitle}]`;
+        mailOptions.text = `Comment. From: ${comment.creator.name}. Body: ${comment.body}`;
+        return mailTransport.sendMail(mailOptions).then(() => {
+          console.log('email sent to:', toEmail);
+        }).catch(error => {
+          console.error('error sending mail:', error);  
+        });
+    }
+  )
+
+  
 });
